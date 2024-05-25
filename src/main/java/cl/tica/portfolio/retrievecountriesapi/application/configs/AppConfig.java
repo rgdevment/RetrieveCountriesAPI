c391c881d4666b3fc80ffb34c946609042060b1d
Mario Hidalgo <mhidalgog@outlook.com>
2024-05-25 01:40:35 -0400

    feat: server compress (#26)
    * feat: server compress
    
    * feat: add cache config

--- src/main/java/cl/tica/portfolio/retrievecountriesapi/application/configs/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package cl.tica.portfolio.retrievecountriesapi.application.configs;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-public class AppConfig {
-}
