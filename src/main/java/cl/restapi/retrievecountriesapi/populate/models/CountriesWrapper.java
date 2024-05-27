0403bf40e72f975c885a82b76f202526ff016d7f
Mario Hidalgo <mhidalgog@outlook.com>
2024-05-27 07:02:14 -0400

    refactor: depreciated population (#28)
    * refactor: depreciated population
    
    * refactor: depreciated population on tests

--- src/main/java/cl/restapi/retrievecountriesapi/populate/models/CountriesWrapper.java
+++
@@ -1,9 +0,0 @@
-package cl.restapi.retrievecountriesapi.populate.models;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.List;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record CountriesWrapper(List<CountryData> countries) {
-}
--- src/main/java/cl/restapi/retrievecountriesapi/populate/models/Flags.java
+++
@@ -1,7 +0,0 @@
-package cl.restapi.retrievecountriesapi.populate.models;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Flags(String png, String svg, String alt) {
-}
