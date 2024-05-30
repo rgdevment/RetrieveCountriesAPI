62134efaed21abaedd9e90d5af7f9dc98438cdbe
rgdevment <mhidalgog@outlook.com>
2024-05-30 15:58:14 -0400

    fix: parameter for open-api

--- src/main/java/cl/restapi/retrievecountriesapi/v1/controllers/StateController.java
+++ src/main/java/cl/restapi/retrievecountriesapi/v1/controllers/StateController.java
@@ -30,7 +30,6 @@ public class StateController {
     }
 
     @GetMapping("/country/{countryCode}")
-    @Parameter(description = "Country code in ISO 3166-1 alpha-2 format")
     @Operation(summary = "Get states by country code",
             description = "Get a list of states by country code in ISO 3166-1 alpha-2 format")
     @ApiResponse(
